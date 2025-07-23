df022fcacab935d302e19c47f1403ac82c3f8690
Aadi Sudan <aadisudan123@gmail.com>
2025-07-22 20:49:29 -0700

    Update load_data.sql

--- scripts/silver/load_data.sql
+++ scripts/silver/load_data.sql
@@ -1,60 +1,31 @@
+-- copying over all our tables from the bronze schema into silver
 DROP TABLE IF EXISTS silver.crm_cust_info;
-create table silver.crm_cust_info(
-	cst_id INT,
-	cst_key VARCHAR(50),
-	cst_firstname VARCHAR(50),
-	cst_lastname VARCHAR(50),
-	cst_marital_status VARCHAR(50),
-	cst_gndr VARCHAR(50),
-	cst_create_date DATE
-);
+CREATE TABLE silver.crm_cust_info AS
+SELECT * FROM bronze.crm_cust_info;
+
 
 DROP TABLE IF EXISTS silver.crm_prd_info;
-create table silver.crm_prd_info(
-	prd_id INT,
-	prd_key VARCHAR(50),
-	prd_nm VARCHAR(50),
-	prd_cost INT,
-	prd_line VARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE
-);
+CREATE TABLE silver.crm_prd_info AS
+SELECT * FROM bronze.crm_prd_info;
+
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
-create table silver.crm_sales_details(
-	sls_ord_num VARCHAR(50),
-	sls_prd_key VARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
-);
+CREATE TABLE silver.crm_sales_details AS
+SELECT * FROM bronze.crm_sales_details;
+
 
 DROP TABLE IF EXISTS silver.erp_cust_az12;
-create table silver.erp_cust_az12(
-	cid VARCHAR(50),
-	bdate DATE,
-	gen VARCHAR(50)
-);
+CREATE TABLE silver.erp_cust_az12 AS
+SELECT * FROM bronze.erp_cust_az12;
+
 
 DROP TABLE IF EXISTS silver.erp_loc_a101;
-create table silver.erp_loc_a101(
-	cid VARCHAR(50),
-	cntry VARCHAR(50)
-);
+CREATE TABLE silver.erp_loc_a101 AS
+SELECT * FROM bronze.erp_loc_a101;
 
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
-create table silver.erp_px_cat_g1v2(
-	id VARCHAR(50),
-	cat VARCHAR(50),
-	subcat VARCHAR(50),
-	maintenance VARCHAR(50)
-);
-
--- after doing this, make sure to load in the data from each independent CSV into the respective table
+CREATE TABLE silver.erp_px_cat_g1v2 AS
+SELECT * FROM bronze.erp_px_cat_g1v2;
 
 select * from silver.crm_cust_info
 -- verify that the data was loaded properly
